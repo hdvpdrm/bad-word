5958b1210ff4ccadf9711c3b285893f147e22b31
Hdvpdrm <hardcoderyan@gmail.com>
2024-11-03 22:51:19 +0300

    added scenes processing, also ability to set background color

--- Window.cpp
+++ Window.cpp
@@ -15,13 +15,37 @@ void GG::Window::run()
 {
   while(!WindowShouldClose())
     {
+       if(current_scene != nullptr)
+	 current_scene->run();
       draw();
     }
 }
 void GG::Window::draw()
 {
   BeginDrawing();
-  ClearBackground(BLACK);
+  
+  if(current_scene != nullptr)
+	 current_scene->draw();
+  
+  ClearBackground(background);
   EndDrawing();
 }
 
+bool GG::Window::does_id_exists(int id)
+{
+  return scenes.find(id) != scenes.end();
+}
+bool GG::Window::add(int id, std::function<BaseScene*()> creator)
+{
+  if(does_id_exists(id)) return false;
+
+  scenes[id] = creator;
+  return true;
+}
+bool GG::Window::set_initial_scene(int id)
+{
+  if(!does_id_exists(id)) return false;
+
+  current_scene = scenes[id]();
+  return true;
+}
--- Window.h
+++ Window.h
@@ -1,18 +1,31 @@
 #ifndef WINDOW_H
 #define WINDOW_H
+#include"BaseScene.h"
 #include<raylib.h>
+#include<unordered_map>
+#include<functional>
 
 namespace GG
 {
 class Window
 {
+ private:
+  BaseScene* current_scene = nullptr;
+  std::unordered_map<int,std::function<BaseScene*()>> scenes;
+
+  Color background = BLACK; //default value
  public:
   Window(const char* title);
   ~Window();
   void run();
   
+  bool add(int id,std::function<BaseScene*()> creator);
+  bool set_initial_scene(int id);
+
+  void set_background_color(Color color){background = color;};
  private:
   void draw();
+  bool does_id_exists(int id);
 };
 };
 
