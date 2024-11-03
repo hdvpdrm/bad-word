b4f6d8f661ff0642ca0f24c779fe2a761676c8b6
Hdvpdrm <hardcoderyan@gmail.com>
2024-11-03 22:51:49 +0300

    base class for each scene in game

---
+++ BaseScene.h
@@ -0,0 +1,25 @@
+#ifndef BASE_SCENE_H
+#define BASE_SCENE_H
+
+namespace GG
+{
+class BaseScene
+{
+protected:
+   bool should_quit = false;
+private:
+  int id, next_id;
+public:
+  BaseScene(int id, int next_id):id(id),next_id(next_id)
+  {}
+  ~BaseScene(){}
+  
+  int get_id()const {return id;}
+  int get_next_id()const {return next_id;}
+
+  virtual void run()=0;
+  virtual void draw()=0;
+
+};
+};
+#endif
