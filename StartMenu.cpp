ef3921a9404333e757a0f3472e7cd0af313669df
Hdvpdrm <hardcoderyan@gmail.com>
2024-11-04 14:14:06 +0300

    made it working as I want

--- StartMenu.cpp
+++ StartMenu.cpp
@@ -4,7 +4,7 @@
 StartMenu::StartMenu(int id, int next_id):GG::BaseScene(id,next_id)
 {
   label_font = LoadFontFromMemory(".ttf", fibberish_ttf, fibberish_ttf_len, 48, NULL, 0);
-  button_font = LoadFontFromMemory(".ttf", fibberish_ttf, fibberish_ttf_len, 32, NULL, 0);
+  button_font = LoadFontFromMemory(".ttf", fibberish_ttf, fibberish_ttf_len, 50, NULL, 0);
 
   if(!IsFontReady(label_font) && !IsFontReady(button_font))
     {
@@ -21,9 +21,57 @@ StartMenu::~StartMenu()
 
 void StartMenu::run()
 {
+  if(lambda_to_choose != 4)
+    {
+      check_keys();
+    }
+  else
+    {
+      if(IsKeyReleased(KEY_SPACE))
+	{
+	  should_quit = true;
+	}
+    }
+
+  
+  for(short i = 0;i<20;++i)
+    {
+      fuck_color.r-=1;
+      fuck_color.g-=1;
+      fuck_color.b-=1;
+    }
 }
 void StartMenu::draw()
 {
   DrawTextEx(label_font,"Bad Word",(Vector2){200,100},48,0,label_color);
-}
 
+  if(lambda_to_choose != 4)
+    DrawTextEx(button_font,get_fuck_to_draw().c_str(),(Vector2){245,240},50,0,fuck_color);
+  else
+    {
+      DrawTextEx(button_font,"press",(Vector2){100,240},50,0,label_color);
+      DrawTextEx(button_font,"SPACE",(Vector2){230,240},50,0,fuck_color);
+      DrawTextEx(button_font,"to play!",(Vector2){370,240},50,0,label_color);
+    }
+}
+std::string StartMenu::get_fuck_to_draw()
+{
+  static const std::string FUCK = "FUCK";
+  std::string out;
+  for(size_t i = 0;i<4;++i)
+    {
+      if(fuck_pressed_checkers[i])out.push_back(FUCK[i]);
+    }
+
+  return out;
+}
+void StartMenu::check_keys()
+{
+  if(key_predicats[lambda_to_choose]())
+    {
+      ++lambda_to_choose;
+      if(lambda_to_choose != 4)
+	fuck_pressed_checkers[lambda_to_choose] = true;
+    }
+
+}
--- StartMenu.h
+++ StartMenu.h
@@ -2,14 +2,31 @@
 #define START_MENU_H
 #include"BaseScene.h"
 #include"font.h"
+#include"Timer.h"
+#include<string>
+#include<functional>
 #include<raylib.h>
 #include<stdio.h>
 
+#define CHECK_KEY_LAMBDA(x) [](){return IsKeyReleased(x);}
 class StartMenu: public GG::BaseScene
 {
  private:
   Font label_font, button_font;
   Color label_color = {5,0,12,255};
+  Color fuck_color = {183,178,190,255};
+  bool fuck_pressed_checkers[4] = {true,false,false,false};
+  const std::function<bool()> key_predicats[4] = {
+    CHECK_KEY_LAMBDA(KEY_F),
+    CHECK_KEY_LAMBDA(KEY_U),
+    CHECK_KEY_LAMBDA(KEY_C),
+    CHECK_KEY_LAMBDA(KEY_K)
+  };
+  short lambda_to_choose = 0;
+  void check_keys();
+  
+  std::string get_fuck_to_draw();
+  
  public:
   StartMenu(int id, int next_id);
   ~StartMenu();
