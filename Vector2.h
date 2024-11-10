bc6ef8ef1d50852768b16e58408bcf7c7b1a0234
Hdvpdrm <hardcoderyan@gmail.com>
2024-11-10 17:02:55 +0300

    added additional overloaded operations

--- Vector2.h
+++ Vector2.h
@@ -9,6 +9,11 @@ struct MyVector2
   MyVector2(T x, T y):x(x),y(y){}
   MyVector2(const MyVector2<T>& vec):x(vec.x),y(vec.y){}
   ~MyVector2(){}
+
+  bool operator==(const MyVector2<T>& element){ return x == element.x && y == element.y;}
+
+  friend MyVector2<T> operator-(const MyVector2<T>& a,const MyVector2<T>& b){ return MyVector2<T>(a.x-b.x,a.y-b.y);}
+  friend MyVector2<T> operator+(const MyVector2<T>& a,const MyVector2<T>& b){ return MyVector2<T>(a.x+b.x,a.y+b.y);}
 };
 typedef MyVector2<int> iVector2;
 typedef MyVector2<float> fVector2;
