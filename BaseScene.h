580dc57f7aeb3031daea7232fe6ef819dd2d8128
Hdvpdrm <hardcoderyan@gmail.com>
2024-11-13 19:58:21 +0300

    added methods to work with returning value

--- BaseScene.h
+++ BaseScene.h
@@ -1,16 +1,22 @@
 #ifndef BASE_SCENE_H
 #define BASE_SCENE_H
+#include<unordered_map>
+#include<string>
 
 namespace GG
 {
 class BaseScene
 {
 protected:
-   bool should_quit = false;
+  bool should_quit = false;
+  std::unordered_map<std::string, std::string> return_value;
 private:
   int id, next_id;
 public:
-  BaseScene(int id, int next_id):id(id),next_id(next_id)
+  BaseScene(int id,
+	    int next_id):
+    id(id),
+    next_id(next_id)
   {}
   ~BaseScene(){}
   
@@ -21,6 +27,15 @@ public:
   virtual void draw()=0;
 
   bool should_change() {return should_quit;}
+
+  std::unordered_map<std::string,std::string> get_return_value(){ return return_value;}
+  void set_return_value(std::unordered_map<std::string,std::string>& ret_val)
+  {
+    for(auto& el:ret_val)
+      {
+	return_value[el.first] = el.second;
+      }
+  }
 };
 };
 #endif
